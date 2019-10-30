9b008a4b045a7577a01b7f413b59237cb0880c4f
eric rodriguez <er@iex.ec>
2019-10-30 16:28:11 +0100

    add cifar file

--- cifar_tf/workflow.sh
+++ cifar_tf/workflow.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+start=$(date +%s)
+
 echo "Number of batches to run " $1
 python cifar10_multi_gpu_train.py -max_steps=100
 echo "training done"
 echo "=============="
 python cifar10_eval.py -run_once
+
+end=$(date +%s)
+
+seconds=$(echo "$end - $start" | bc)
+echo $seconds' sec'
+
+echo 'Formatted:'
+awk -v t=$seconds 'BEGIN{t=int(t*1000); printf "%d:%02d:%02d\n", t/3600000, t/60000%60, t/1000%60}'
